eb809cf03e33a1a92493cdb1ec5218f1358d66d5
zhou.han <zhou.han@yuancore.com>
2022-03-04 16:36:24 +0800

    修复fiber listen

--- outer/rest/controller.go
+++ outer/rest/controller.go
@@ -3,12 +3,20 @@ package rest
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/qyqx233/go-tunel/outer/pub"
+	"github.com/rs/zerolog/log"
 )
 
 func StartRest(addr string) {
+	log.Debug().Str("addr", addr).Msg("监听地址")
 	app := fiber.New()
 	app.Get("/rest/memStor", func(c *fiber.Ctx) error {
 		return c.JSON(&pub.MemStor)
 	})
-	app.Server().ListenAndServe(addr)
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("hello")
+	})
+	err := app.Listen(addr)
+	if err != nil {
+		log.Error().Err(err).Msg("fiber listen error")
+	}
 }
